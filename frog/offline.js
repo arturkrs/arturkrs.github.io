4217e221f14d726b28449ab91d9079c189df2fc3
Artur K <41274376+arturkrs@users.noreply.github.com>
2018-08-17 11:04:56 -0300

    asd

---
+++ frog/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1534514624,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/frog-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/tongue_tip-sheet0.png",
+		"images/tongue-sheet0.png",
+		"images/object-sheet0.png",
+		"images/goal-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
